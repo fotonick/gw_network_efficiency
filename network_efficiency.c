e617d287503d12bb2309e5ce2240c9474c131586
Justin Wagner <justin.wagner@ligo.org>
2012-07-30 10:06:18 -0700

    X2 rho2 join sampling written but unimplemented as of yet.

--- network_efficiency.c
+++ network_efficiency.c
@@ -29,16 +29,57 @@
 #include <lal/LALSimNoise.h>
 #include <lal/LALConstants.h>
 #include <lal/LALDetectors.h>
+#include <time.h>
 
-int debug=1;
+int debug=0;
 
-ssize_t str2network(LALDetector[],double[],char*);
-static double horizon_distance(double, double, double, double, double, char);
-static double rchisq_2(gsl_rng*, double);
-void Ssq(double*, gsl_rng*, double, double*, ssize_t);
+ssize_t str2network(LALDetector network[], double reach[], char *str);
+static double horizon_distance(double m1, double m2, double f_low, double f_high, double snr_thresh, char detector);
+static double rchisq_2(gsl_rng *rng, double lambda);
+void Ssq(double *S2, gsl_rng *rng, double beam_fac, double *response, ssize_t network_size);
 
-int main(int argc, char *argv[]) {
-  const static double twopi = 2 * M_PI;
+void wave_Consts_Test(double *rho2, double *X2, gsl_rng *rng, double lambda){
+  /*Takes two pointers, rho2 and X2, and sets the values to random
+    rho2: X^2, df=2, ncp=lambda
+    X2: X^2, df=2p-2, ncp=0 
+    For use as wave consistancy test from Creighton/Anderson
+    Sec 7.8.1.4  */
+
+  const static int p=16;
+  const double sigma=1;
+  const double mu=sigma*sqrt(lambda/(2*p*p));
+  double a[p]; double b[p];
+  double asum=0; double bsum=0;
+  *X2=0;
+
+  for (int i=0; i<p; i++){
+    a[i]=gsl_ran_gaussian(rng, sigma/sqrt(p))+mu;
+    b[i]=gsl_ran_gaussian(rng, sigma/sqrt(p))+mu;
+    asum+=a[i]; bsum+=b[i];
+  }
+
+  for (int i=0; i<p; i++)
+    *X2+=(pow(asum/p-a[i],2) + pow(bsum/p-b[i],2)) * p/pow(sigma,2);
+
+  *rho2=asum*asum+bsum*bsum;
+}
+
+
+int main(int argc, char *argv[]) {  
+
+  gsl_rng *x2rng=gsl_rng_alloc(gsl_rng_mt19937);
+  gsl_rng_set(x2rng, 600);
+  
+  double a;//=malloc(sizeof(double));
+  double b;//=malloc(sizeof(double));
+    
+  for (int z=0; z<1000000; z++){
+    wave_Consts_Test(&a, &b, x2rng, 5.0);
+    printf("%f %f\n", a, b);
+  }  
+
+  const clock_t beginTimeStamp=clock();
+  const static double twopi=2 * M_PI;
   LALDetector network[LAL_NUM_DETECTORS];
   double Dhor[LAL_NUM_DETECTORS];
   double thresh_snr;
@@ -56,62 +97,82 @@ int main(int argc, char *argv[]) {
 
   /*Initilize network and other cmdline arguments*/
   memset(network, 0, LAL_NUM_DETECTORS);
-  thresh_snr = strtod(argv[1], NULL);
-  network_size = str2network(network, Dhor, argv[2]);
+  thresh_snr=strtod(argv[1], NULL);
+  network_size=str2network(network, Dhor, argv[2]);
   if (network_size == -1) exit(2);
-  N = strtol(argv[3], NULL, 10);
-  beam_fac = 1 - cos(strtod(argv[4], NULL) * M_PI / 180.);
+  N=strtol(argv[3], NULL, 10);
+  beam_fac=1 - cos(strtod(argv[4], NULL) * M_PI / 180.);
 
   /*Set up/initialize the distance parameters*/
   D_max=strtol(argv[5], NULL, 10);
   D_steps=strtol(argv[6], NULL, 10);
-  double d_step=(D_max-1)/(D_steps-1);
+  const double d_step=((double)(D_max-1))/(D_steps-1);
 
   /*Initialize final storage array of efficiency for each distance step*/
-  double *efficiency = calloc(D_steps, sizeof(double));
+  double *efficiency=calloc(D_steps, sizeof(double));
 
   //START THREAD HERE
-  /*Create and seed random number generator*/
-  gsl_rng *rng = gsl_rng_alloc(gsl_rng_mt19937);
-  gsl_rng_set(rng, 0);
-  double *response = malloc(2 * network_size * sizeof(double));
+  #pragma omp parallel
+  {
+    /*Create and seed random number generator*/
+    gsl_rng *rng=gsl_rng_alloc(gsl_rng_mt19937);
+    #ifdef _OPENMP
+    gsl_rng_set(rng, omp_get_thread_num());//Seed with thread number.
+    #else
+    gsl_rng_set(rng, 0);
+    #endif
 
-  /*Total detections for each distance step for the given thread.*/
-  long *threadTotals = calloc(D_steps, sizeof(long));
+    double *response=malloc(2 * network_size * sizeof(double));
 
-  /*Preallocate variables needed inside loops*/
-  size_t j, k, l;
-  double S2[network_size];
-  /*size_t objects are inherently the size of a pointer on the system.*/
-  for (k=1; k<=N; k++) {
-    /* draw orientations and determine network response */
-    const double lon=twopi*gsl_rng_uniform(rng);
-    const double lat=M_PI_2-acos(2*gsl_rng_uniform(rng)-1);
-    const double zeta=twopi*gsl_rng_uniform(rng);
+    /*Total detections for each distance step for the given thread.*/
+    long *threadTotals=calloc(D_steps, sizeof(long));
 
-    for (l = network_size; l--;)
-      XLALComputeDetAMResponse(response + 2 * l, response + 2 * l + 1, network[l].response, lon, lat, zeta, 0.);
+    /*Preallocate variables needed inside loops*/
+    size_t j, k, l;
+    /*size_t objects are inherently the size of a pointer on the system.*/
+    
+    #pragma omp for schedule(static)
+    for(k=1; k<=N; k++){
+      /* draw orientations and determine network response */
+      const double lon=twopi*gsl_rng_uniform(rng);
+      const double lat=M_PI_2 - acos(2*gsl_rng_uniform(rng) - 1);
+      const double zeta=twopi*gsl_rng_uniform(rng);
 
-    Ssq(S2, rng, beam_fac, response, network_size);
+      for(l=network_size; l--;)
+	XLALComputeDetAMResponse(response+2*l, response+2*l+1, network[l].response, lon, lat, zeta, 0.0);
 
-    for(j=D_steps;j--;){
-      const double d = 1. + j * d_step;
-      int successes = 0;
-      for(l=network_size;l--;) {
-          const double lambda = (64-2)*(Dhor[l]/d)*(Dhor[l]/d)*S2[l];
-          successes += rchisq_2(rng, lambda) > 64;
+      double S2[network_size];
+      Ssq(S2, rng, beam_fac, response, network_size);
+
+      for(j=D_steps; j--;){
+	const double d=1.0+j*d_step;
+	int successes=0;
+	for (l=network_size; l--;) {
+          const double lambda=(thresh_snr*thresh_snr-2)*(Dhor[l]/d)*(Dhor[l]/d)*S2[l];
+          successes+=rchisq_2(rng, lambda)>64;
+	}
+	threadTotals[j]+=(successes>=2);
       }
-      threadTotals[j]+=successes>=2;
     }
+
+    //ATOMIC MOVE FROM threadTotals TO efficiency
+    #pragma omp critical
+    for(j=D_steps; j--;)
+      efficiency[j]+=threadTotals[j]/((double)N);
+
+    gsl_rng_free(rng);
+    free(threadTotals);
+    free(response);
+    //END THREAD HERE
   }
-
-  printf("This is before the seg fault.\n");
-  gsl_rng_free(rng);
-  printf("This is after the seg fault.\n");
-  free(threadTotals);
-  free(response);
-  //END THREAD HERE
-
+  
+  if(debug){
+    size_t i;
+    printf("DISTANCE  EFFICIENCY\n");
+    for(i=0; i<D_steps; i++)
+      printf("%09.5f %09.5f\n", 1.0+i*d_step, 100*efficiency[i]);
+  }
+  
   free(efficiency);
 
   return 0;
@@ -124,10 +185,12 @@ int main(int argc, char *argv[]) {
 /////#/////#####//#///#/////
 
 static double rchisq_2(gsl_rng *rng, double lambda) {
-  double a = sqrt(0.5 * lambda);
-  const double temp = gsl_ran_gaussian(rng, 1.0) + a;
-  const double temp2 = gsl_ran_gaussian(rng, 1.0) + a;
-  return temp * temp + temp2 * temp2;
+  /*Generates a random value from a degree 2 
+    chi_squared with non-centrality parameter of lambda*/
+  double a=sqrt(0.5*lambda);
+  const double temp=gsl_ran_gaussian(rng, 1.0)+a;
+  const double temp2=gsl_ran_gaussian(rng, 1.0)+a;
+  return (temp*temp)+(temp2*temp2);
 }
 
 void Ssq(double *S2, gsl_rng *rng, double beam_fac, double *response, ssize_t network_size){
@@ -139,43 +202,41 @@ void Ssq(double *S2, gsl_rng *rng, double beam_fac, double *response, ssize_t ne
   size_t l=network_size;
   for(;l--;) {
     double fplus=response[2*l], fcross=response[2*l+1];
-    S2[l]=fplus * fplus * iotafac + fcross * fcross * cosiotasq; //S^2
+    S2[l]=(fplus*fplus)*iotafac+(fcross*fcross)*cosiotasq;
   }
 }
 
-/*
- * Convert string like "HLVK" to an array of LALDetectors.
- * Return the size of the network.
- */
 ssize_t str2network(LALDetector network[LAL_NUM_DETECTORS], double reach[LAL_NUM_DETECTORS], char *str) {
-  size_t k = 0;
+  /*Convert string like "HLVK" to an array of LALDetectors.
+  Return the size of the network.*/
+  size_t k=0;
   while (k < LAL_NUM_DETECTORS && str[k]) {
     /* WARNING masses, frequency cutoffs, and SNR threshold hardcoded! */
-    reach[k] = horizon_distance(1.4, 1.4, 40., 1570., 8., str[k]);
+    reach[k]=horizon_distance(1.4, 1.4, 10., 1570., 8., str[k]);
     /* fprintf(stderr, "Detector '%c' horizon distance: %g\n", str[k], reach[k]); */
-    if (str[k] == 'H') {
-      network[k++] = lalCachedDetectors[LAL_LHO_4K_DETECTOR];
+    if (str[k]=='H') {
+      network[k++]=lalCachedDetectors[LAL_LHO_4K_DETECTOR];
     }
-    else if (str[k] == 'L') {
-      network[k++] = lalCachedDetectors[LAL_LLO_4K_DETECTOR];
+    else if (str[k]=='L') {
+      network[k++]=lalCachedDetectors[LAL_LLO_4K_DETECTOR];
     }
-    else if (str[k] == 'V') {
-      network[k++] = lalCachedDetectors[LAL_VIRGO_DETECTOR];
+    else if (str[k]=='V') {
+      network[k++]=lalCachedDetectors[LAL_VIRGO_DETECTOR];
     }
-    else if (str[k] == 'K') {
+    else if (str[k]=='K') {
       /* numbers from private communication with Koji Arai */
-      LALDetector *detector = network + k;
-      LALFrDetector *frDetector = &(detector->frDetector);
+      LALDetector *detector=network+k;
+      LALFrDetector *frDetector=&(detector->frDetector);
       strncpy(frDetector->name, "KAGRA", LALNameLength);
       strncpy(frDetector->prefix, "K1", 3);
-      frDetector->vertexLatitudeRadians = 2.396511595913414;
-      frDetector->vertexLongitudeRadians = 0.6354743806511354;
-      frDetector->vertexElevation = 372.0;
-      frDetector->xArmAltitudeRadians = 0.;
-      frDetector->xArmAzimuthRadians = 1.076693615555302;
-      frDetector->yArmAltitudeRadians = 0.;
-      frDetector->yArmAzimuthRadians = 5.789082595939991;
-      detector = XLALCreateDetector(network + k, frDetector, LALDETECTORTYPE_IFODIFF);
+      frDetector->vertexLatitudeRadians=2.396511595913414;
+      frDetector->vertexLongitudeRadians=0.6354743806511354;
+      frDetector->vertexElevation=372.0;
+      frDetector->xArmAltitudeRadians=0.0;
+      frDetector->xArmAzimuthRadians=1.076693615555302;
+      frDetector->yArmAltitudeRadians=0.0;
+      frDetector->yArmAzimuthRadians=5.789082595939991;
+      detector=XLALCreateDetector(network+k, frDetector, LALDETECTORTYPE_IFODIFF);
       if (!detector) {
         fprintf(stderr, "Failed to create KAGRA detector\n");
         return -1;
@@ -184,18 +245,18 @@ ssize_t str2network(LALDetector network[LAL_NUM_DETECTORS], double reach[LAL_NUM
     }
     else if (str[k] == 'I') {
       /* numbers from Schutz 2011 network FOMs */
-      LALDetector *detector = network + k;
-      LALFrDetector *frDetector = &(detector->frDetector);
+      LALDetector *detector=network+k;
+      LALFrDetector *frDetector=&(detector->frDetector);
       strncpy(frDetector->name, "Indigo", LALNameLength);
       strncpy(frDetector->prefix, "I1", 3);
-      frDetector->vertexLatitudeRadians = 1.3098647554849334;
-      frDetector->vertexLongitudeRadians = 0.33329486135237268;
-      frDetector->vertexElevation = 0.0;
-      frDetector->xArmAltitudeRadians = 0.;
-      frDetector->xArmAzimuthRadians = 3.9269908169872414;
-      frDetector->yArmAltitudeRadians = 0.;
-      frDetector->yArmAzimuthRadians = 5.497787143782138;
-      detector = XLALCreateDetector(network + k, frDetector, LALDETECTORTYPE_IFODIFF);
+      frDetector->vertexLatitudeRadians=1.3098647554849334;
+      frDetector->vertexLongitudeRadians=0.33329486135237268;
+      frDetector->vertexElevation=0.0;
+      frDetector->xArmAltitudeRadians=0.0;
+      frDetector->xArmAzimuthRadians=3.9269908169872414;
+      frDetector->yArmAltitudeRadians=0.0;
+      frDetector->yArmAzimuthRadians=5.497787143782138;
+      detector=XLALCreateDetector(network+k, frDetector, LALDETECTORTYPE_IFODIFF);
       if (!detector) {
         fprintf(stderr, "Failed to create Indigo detector\n");
         return -1;
@@ -211,45 +272,43 @@ ssize_t str2network(LALDetector network[LAL_NUM_DETECTORS], double reach[LAL_NUM
 }
 
 
-/* Compute horizon distance for a given detector.  After equation (2) from LLOID paper. */
-static double horizon_distance(double m1, double m2, double f_low, double f_high, double snr_thresh, char detector)
-{
+static double horizon_distance(double m1, double m2, double f_low, double f_high, double snr_thresh, char detector){
+  /* Compute horizon distance for a given detector.  After equation (2) from LLOID paper. */
   double (*S)(double);
   double Dhor, f;
 
   /* Chirp mass in seconds */
-  const double Mchirp = LAL_MTSUN_SI * pow(pow(m1 * m2, 3) / (m1 + m2), 0.2);
+  const double Mchirp=LAL_MTSUN_SI*pow(pow(m1*m2, 3)/(m1+m2), 0.2);
 
   /* Integration step in Hz */
-  static const double df = 0.01;
+  static const double df=0.02;
 
   /* Select the function to use for sampling the PSD */
-  switch (detector)
-    {
+  switch (detector){
     case 'H':
     case 'L':
     case 'I':
-      S = XLALSimNoisePSDaLIGOZeroDetHighPower;
+      S=XLALSimNoisePSDaLIGOZeroDetHighPower;
     break;
     case 'V':
-      S = XLALSimNoisePSDAdvVirgo;
+      S=XLALSimNoisePSDAdvVirgo;
       break;
     case 'K':
-      S = XLALSimNoisePSDKAGRA;
+      S=XLALSimNoisePSDKAGRA;
       break;
     default:
       fprintf(stderr, "This line should never be reached.\n");
       exit(EXIT_FAILURE);
-    }
+  }
 
-  for (Dhor = 0, f = f_low; f < f_high; f += df)
-    Dhor += pow(f, -7./3) / S(f) * df;
+  for (Dhor=0, f=f_low; f<f_high; f+=df)
+    Dhor+=pow(f, (-7.0/3))/S(f)*df;
 
-  Dhor = sqrt(5./6 * Dhor);
-  Dhor *= pow(Mchirp, 5./6) * pow(M_PI, -2./3) * LAL_C_SI / snr_thresh;
+  Dhor=sqrt((5./6)*Dhor);
+  Dhor*=pow(Mchirp, (5./6))*pow(M_PI, (-2./3))*LAL_C_SI/snr_thresh;
 
   /* Convert from m to Mpc */
-  Dhor /= 1e6 * LAL_PC_SI;
+  Dhor/=1e6*LAL_PC_SI;
 
   return Dhor;
 }
